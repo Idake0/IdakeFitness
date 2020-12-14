73a4adac76489f7a9b39423e883c0915b8968fb4
Kirill <da.kirilldnevnik@gmail.com>
2020-12-14 22:22:54 +0800

    Создана модель пользователя.

---
+++ IdakeFitness.BL/Model/Gender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace IdakeFitness.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Имя пола.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name"> Пол. </param>
+        public Gender(string name)
+        {
+            Name = name ?? throw new ArgumentNullException("Имя гендера не может быть пустым или null" ,nameof(name));
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+}
